81fe97a0ea3c6dbbdcdba541ae99dd75edd479ff
Sasha Murray <sashakmurray@gmail.com>
2023-02-22 14:03:29 -0500

    Initial Commit

---
+++ Drive+/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Drive+
+//
+//  Created by Sasha Murray (student LM) on 2/22/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drive+/Drive_App.swift
@@ -0,0 +1,17 @@
+//
+//  Drive_App.swift
+//  Drive+
+//
+//  Created by Sasha Murray (student LM) on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Drive_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
