d254d55836848729dd224fee433a7f295450bee2
Sasha Murray <sashakmurray@gmail.com>
2023-03-09 12:55:36 -0500

    Rename data to content

--- Drive+/Model/Document.swift
+++ Drive+/Model/Document.swift
@@ -8,9 +8,10 @@
 import Foundation
 
 class Document: File {
-    var data: String?
+    var content: String
     
-    init(data: String = "hello") {
-        self.data = data
+    init(name: String = "File1", created: Date = Date.now, last_modified: Date = Date.now, type: Type = .document, content: String = "hello") {
+        self.content = content
+        super.init(name: name, created: created, last_modified: last_modified, type: type)
     }
 }
--- Drive+/Model/Folder.swift
+++ Drive+/Model/Folder.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 class Folder: File, ObservableObject {
-    var data: [File]
+    var content: [File]
     
-    init(name: String = "File1", created: Date = Date.now, last_modified: Date = Date.now, type: Type = .folder, data: [File] = []) {
-        self.data = data
+    init(name: String = "File1", created: Date = Date.now, last_modified: Date = Date.now, type: Type = .folder, content: [File] = []) {
+        self.content = content
         super.init(name: name, created: created, last_modified: last_modified, type: type)
     }
 }
