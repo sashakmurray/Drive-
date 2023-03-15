b8fa64605de334e87f67086346c44e72d13de0fc
Sasha Murray <sashakmurray@gmail.com>
2023-03-15 13:31:22 -0400

    Make UIKit versions of fonts

--- Drive+/Model/Constants.swift
+++ Drive+/Model/Constants.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
-struct Fonts {
-    static let small: Font = Font(UIFont(name: "HelveticaNeue-Thin", size: 16) ?? UIFont.systemFont(ofSize: 16))
-    static let medium: Font = Font(UIFont(name: "HelveticaNeue-Thin", size: 20) ?? UIFont.systemFont(ofSize: 16))
-    static let large: Font = Font(UIFont(name: "HelveticaNeue-Thin", size: 25) ?? UIFont.systemFont(ofSize: 16))
+struct UIFonts {
+    static let small: UIFont = UIFont(name: "HelveticaNeue-Thin", size: 16) ?? UIFont.systemFont(ofSize: 16)
+    static let medium: UIFont = UIFont(name: "HelveticaNeue-Thin", size: 20) ?? UIFont.systemFont(ofSize: 16)
+    static let large: UIFont = UIFont(name: "HelveticaNeue-Thin", size: 25) ?? UIFont.systemFont(ofSize: 16)
+}
 
+struct Fonts {
+    static let small = Font(UIFonts.small as CTFont)
+    static let medium = Font(UIFonts.medium as CTFont)
+    static let large = Font(UIFonts.large as CTFont)
 }
